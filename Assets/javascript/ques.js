ef5e2e803e045f8878c0ac6bcc0378de74ea3e19
Ezkenneth <kennethjoeezigbo@gmail.com>
2020-12-05 21:11:14 -0500

    cannot complete project - booking support session

---
+++ Assets/javascript/ques.js
@@ -0,0 +1,69 @@
+var quesList = [
+
+{
+    question: "What is H1 in an html document?"
+    options: ["element", "object", "value"]
+    ans: "element"
+
+},
+{
+    question: "What does the # represent"
+    options: ["class", "id", "object"]
+    ans: "id"   
+},
+{
+    question: "What does one use to generate a random number?"
+    options: ["number.random", "math.random", "math.guess"]
+    ans: "math.random"
+},
+{
+    question: "What is a jumbotron?"
+    options: ["boostrap-component", "boottraps", "element"]
+    ans: "bootstrap-element"
+},
+{
+    question: "What is it called when you fix up code to make it more readable?"
+    options: ["debugging", "restructuring", "refactoring"]
+    ans: "refactoring"
+},
+{
+    question: "The design of elements is typically called the?"
+    options: ["style", "script", "mature"]
+    ans: "math.random"
+}
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ]
+// var correctAns= ["Jquery", "Bootstrap", "CSS", "Template Literals", "Object", "Math Random", "Components", "Element"]
