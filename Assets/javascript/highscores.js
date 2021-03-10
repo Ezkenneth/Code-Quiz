861c5ccf69115fa271db7744a9d123fceabb5829
Ezkenneth <kennethjoeezigbo@gmail.com>
2021-03-10 16:22:40 -0500

    re-configuring application

--- Assets/javascript/highscores.js
+++
@@ -1,8 +0,0 @@
-// Populating the highscores
-const highScoresList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-
-highScoresList.innerHTML =
-highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join('')
